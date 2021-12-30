30da08c093f2dda868f6a29d868be4b63f491723
Kevin <TaunTaunChewbacca@gmail.com>
2021-12-30 01:18:38 -0500

    Initial commit of working commands.

---
+++ src/main/java/me/tauntaunchewie/PoopCommander.java
@@ -0,0 +1,64 @@
+package me.tauntaunchewie;
+
+import me.tauntaunchewie.utils.PoopUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.logging.Logger;
+
+public class PoopCommander implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        Logger logger = Logger.getLogger("Minecraft");
+
+        // Make sure a player is calling this
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            // Check if user has permission to execute this
+            if (player.hasPermission("poopplugin.set")) {
+                // Make sure we have exactly 2 args
+                if (args.length == 1) {
+
+                    if (args[0].equalsIgnoreCase("on")) {
+                        // Turn shif pooping on
+                        PoopUtils.setPoopOn(true);
+                        player.sendMessage("Pooping turned ON");
+                    }
+                    else if (args[0].equalsIgnoreCase("off")) {
+                        // Turn shift pooping off
+                        PoopUtils.setPoopOn(false);
+                        player.sendMessage("Pooping turned OFF");
+                    }
+                    else if (args[0].equalsIgnoreCase("status")) {
+                        player.sendMessage("Pooping status is: " + (PoopUtils.poopOn() ? "ON" : "OFF"));
+                    }
+                    else {
+                        player.sendMessage("Usage: /poop (on or off)");
+                        player.sendMessage("Example: /poop on");
+                    }
+                }
+                else {
+                    player.sendMessage("Usage: /poop (on or off or status)");
+                    player.sendMessage("Example: /poop on");
+                }
+            }
+            else {
+                player.sendMessage("You do not have permission to execute this command");
+            }
+
+            return true;
+        }
+        else {
+            // Only allow players to invoke
+            return false;
+        }
+    }
+
+    public PoopCommander(PoopPlugin plugin) {
+        PoopUtils.loadDefaults(plugin);
+    }
+}
